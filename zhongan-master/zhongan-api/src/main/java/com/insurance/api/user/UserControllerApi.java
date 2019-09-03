01547d2b3f76ba3ba7c48ff3f8b9cf737a1f4aec
1zhangsan <2309937822@qq.com>
2019-09-03 23:15:31 +0800

    删除了一些冗余模块

--- zhongan-master/zhongan-api/src/main/java/com/insurance/api/user/UserControllerApi.java
+++ zhongan-master/zhongan-api/src/main/java/com/insurance/api/user/UserControllerApi.java
@@ -1,8 +1,16 @@
 package com.insurance.api.user;
 
 import com.insurance.pojo.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
+@Api(value="课程管理接口",description = "课程管理接口，提供课程的增、删、改、查")
 public interface UserControllerApi {
 
+
+    @ApiOperation("课程计划查询")
     public User getOne();
+
+    @ApiOperation("课程计划查询")
+    public User login(User user);
 }
