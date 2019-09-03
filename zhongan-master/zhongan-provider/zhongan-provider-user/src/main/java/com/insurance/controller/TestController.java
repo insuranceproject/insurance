e26af064c65a13320178ed96a56e4af9e4e2f3f1
1zhangsan <2309937822@qq.com>
2019-09-04 04:30:12 +0800

    添加了分布式session,更改了端口号,调用时请更改端口号,补提交

---
+++ zhongan-master/zhongan-provider/zhongan-provider-user/src/main/java/com/insurance/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.insurance.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/session")
+    public Object getsession(HttpSession session){
+        System.out.println(session.getId());
+        return session.getId();
+    }
+
+}
