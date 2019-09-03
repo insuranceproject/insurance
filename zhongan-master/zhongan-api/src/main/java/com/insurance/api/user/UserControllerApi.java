f28ba1b502f8ea726af3ff174ec2408f1f031fd1
qingyumian <1206862561@qq.com>
2019-09-04 02:07:58 +0800

    更新了API接口 更新了模块名

--- zhongan-master/zhongan-api/src/main/java/com/insurance/api/user/UserControllerApi.java
+++ zhongan-master/zhongan-api/src/main/java/com/insurance/api/user/UserControllerApi.java
@@ -11,6 +11,35 @@ public interface UserControllerApi {
     @ApiOperation("课程计划查询")
     public User getOne();
 
-    @ApiOperation("课程计划查询")
+    /**
+     * 个人登陆
+     * @param user
+     * @return
+     */
+    @ApiOperation("个人登陆")
     public User login(User user);
+
+    /**
+     * 个人注册
+     * @param user
+     * @return
+     */
+    @ApiOperation("个人注册")
+    public boolean registered(User user);
+
+    /**
+     * 个人修改密码
+     * @param user
+     * @return
+     */
+    @ApiOperation("个人修改密码")
+    public boolean updateUserPassWord(User user);
+
+    /**
+     * 修改个人信息
+     * @param user
+     * @return
+     */
+    @ApiOperation("修改个人信息")
+    public boolean updateUser(User user);
 }
