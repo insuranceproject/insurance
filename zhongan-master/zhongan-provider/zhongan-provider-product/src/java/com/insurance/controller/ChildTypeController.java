09bb1b870c1f7eed584ffddd4a669fa99b9bca8c
1zhangsan <2309937822@qq.com>
2019-09-05 11:27:10 +0800

    更新了商品api,新增了子分类api,补提交

--- zhongan-master/zhongan-provider/zhongan-provider-product/src/java/com/insurance/controller/ChildTypeController.java
+++ zhongan-master/zhongan-provider/zhongan-provider-product/src/java/com/insurance/controller/ChildTypeController.java
@@ -5,17 +5,21 @@ import com.insurance.api.childtype.ChildTypeControllerApi;
 import com.insurance.pojo.Childtype;
 import com.insurance.service.ChildTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/childType")
 public class ChildTypeController implements ChildTypeControllerApi {
 
     @Autowired
     private ChildTypeService childTypeService;
 
     @Override
+    @GetMapping("findByParId")
     public List<Childtype> findByParId(Integer parId) {
         return childTypeService.list(new QueryWrapper<Childtype>().eq("parent_id",parId));
     }
